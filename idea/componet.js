7fd2aee85fe15cd901aa34c26df75961d91ebe2d
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-09-17 18:41:55 +0900

    Update componet.js

--- idea/componet.js
+++ idea/componet.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'React';
+import React, { useState, useEffect } from 'react';
 import { useJutsu } from '../hooks/useJutsu';
-import JutsuList from '../components/JutsuList.vue';
-import Chakra from '../components/ChakraEffect';
+import JutsuList from '../components/JutsuList';
+import ChakraEffect from '../components/ChakraEffect';
 
 export default function JutsuCatalog() {
   const [jutsuList, setJutsuList] = useState([]);
@@ -13,11 +13,11 @@ export default function JutsuCatalog() {
       setJutsuList(data);
     }
     loadJutsuList();
-  }, []);
+  }, [fetchJutsuList]);
 
   return (
     <div className="jutsu-catalog">
-      <h1 className="ninja-font">忍術カカタログ</h1>
+      <h1 className="ninja-font">忍術カタログ</h1>
       <ChakraEffect />
       <JutsuList jutsuList={jutsuList} />
     </div>
